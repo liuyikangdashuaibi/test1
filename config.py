97957101f3d88c06f2ba98c22d39014b75be02b6
liuyikangdashuaibi <47075256+liuyikangdashuaibi@users.noreply.github.com>
2020-07-24 17:55:27 +0800

    更新page test文件，同时添加配置文件

---
+++ config.py
@@ -0,0 +1,16 @@
+
+#一部分配置文件
+class Config:
+
+    #将浏览器类型设置为火狐浏览器
+    browser = "firefox"
+
+    # 配置运行的 URL
+    url = "www.bilibili.com"
+
+    # 失败重跑次数
+    rerun = "1"
+
+    # 当达到最大失败数，停止执行
+    max_fail = "5"
+
--- page.py
+++ page.py
@@ -1,7 +1,11 @@
 from page_objects import Page,PageElement,PageElements
 
 class BiliBili(Page):
+
+    #根据html元素找到输入关键字的元素
     input=PageElement(class_name="nav-search-keyword",describe="搜索")
+
+    #根据html元素找到点击的按钮
     button=PageElement(class_name="bilifont.bili-icon",describe="按钮")
 
 
